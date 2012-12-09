c3514259c26279636d6be347bec6e3e9fd68704e
Shafiul Azam <ishafiul@gmail.com>
2012-12-09 10:57:46 +0600

    minor fixes

--- scripts/Minesweeper.js
+++ scripts/Minesweeper.js
@@ -67,13 +67,13 @@ Minesweeper.prototype._init = function(){
     $('#flagAvailable').html( this.numMines );
     
     // Preload some images
-    var imgArr = ['flag.jpg', 'empty.jpg', 'bomb.jpg'];
-    var imgBase = 'style/img/';
-    var imgObArr = new Array();
-    for(i=0; i<imgArr.length; i++ ){
-        imgObArr[i] = new Image();
-        imgObArr[i].src = imgBase + imgArr[i];
-    }
+//    var imgArr = ['flag.jpg', 'empty.jpg', 'bomb.jpg'];
+//    var imgBase = 'style/img/';
+//    var imgObArr = new Array();
+//    for(i=0; i<imgArr.length; i++ ){
+//        imgObArr[i] = new Image();
+//        imgObArr[i].src = imgBase + imgArr[i];
+//    }
 }
 
 /**
